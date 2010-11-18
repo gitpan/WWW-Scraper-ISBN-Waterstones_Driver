99410227df29cfcb62384a20413a5e5fc7e8420a
Barbie <barbie@missbarbell.co.uk>
2010-11-18 14:44:01 +0000

    Import of BARBIE/WWW-Scraper-ISBN-Waterstones_Driver-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-Waterstones_Driver
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-Waterstones_Driver-0.01.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Scraper/ISBN/Waterstones_Driver.pm
@@ -0,0 +1,303 @@
+package WWW::Scraper::ISBN::Waterstones_Driver;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION @ISA);
+$VERSION = '0.01';
+
+#--------------------------------------------------------------------------
+
+=head1 NAME
+
+WWW::Scraper::ISBN::Waterstones_Driver - Search driver for the Waterstones online book catalog.
+
+=head1 SYNOPSIS
+
+See parent class documentation (L<WWW::Scraper::ISBN::Driver>)
+
+=head1 DESCRIPTION
+
+Searches for book information from Waterstones online book catalog.
+
+=cut
+
+#--------------------------------------------------------------------------
+
+###########################################################################
+# Inheritence
+
+use base qw(WWW::Scraper::ISBN::Driver);
+
+###########################################################################
+# Modules
+
+use WWW::Mechanize;
+
+###########################################################################
+# Constants
+
+use constant	REFERER	=> 'http://www.waterstones.com';
+use constant	SEARCH	=> 'http://www.waterstones.com/waterstonesweb/simpleSearch.do?typeAheadFormSubmit.x=15&typeAheadFormSubmit.y=17&simpleSearchString=';
+my ($URL1,$URL2) = ('http://www.waterstones.com/book/','/[^?]+\?b=\-3\&amp;t=\-26\#Bibliographicdata\-26');
+
+#--------------------------------------------------------------------------
+
+###########################################################################
+# Public Interface
+
+=head1 METHODS
+
+=over 4
+
+=item C<search()>
+
+Creates a query string, then passes the appropriate form fields to the 
+Book Depository server.
+
+The returned page should be the correct catalog page for that ISBN. If not the
+function returns zero and allows the next driver in the chain to have a go. If
+a valid page is returned, the following fields are returned via the book hash:
+
+  isbn          (now returns isbn13)
+  isbn10        
+  isbn13
+  ean13         (industry name)
+  author
+  title
+  book_link
+  image_link
+  thumb_link
+  description
+  pubdate
+  publisher
+  binding       (if known)
+  pages         (if known)
+
+The book_link, image_link and thumb_link all refer back to the Waterstones
+website.
+
+=cut
+
+sub search {
+	my $self = shift;
+	my $isbn = shift;
+	$self->found(0);
+	$self->book(undef);
+
+    # validate and convert into EAN13 format
+    my $ean = $self->convert_to_ean13($isbn);
+    return $self->handler("Invalid ISBN specified")   
+        if(!$ean || (length $isbn == 13 && $isbn ne $ean)
+                 || (length $isbn == 10 && $isbn ne $self->convert_to_isbn10($ean)));
+
+	my $mech = WWW::Mechanize->new();
+    $mech->agent_alias( 'Windows IE 6' );
+    $mech->add_header( 'Accept-Encoding' => undef );
+    $mech->add_header( 'Referer' => REFERER );
+
+    eval { $mech->get( SEARCH . $ean ) };
+    return $self->handler("The Waterstones website appears to be unavailable.")
+	    if($@ || !$mech->success() || !$mech->content());
+
+    my $content = $mech->content;
+    my ($link,$thumb) = $content =~ m!<a href="([^"]+)">\s*<img src="([^"]+$ean.jpg)"!si;
+
+#print STDERR "\n# search=[".SEARCH."$ean]\n";
+#print STDERR "\n# link=[$link]\n";
+#print STDERR "\n# thumb=[$thumb]\n";
+#print STDERR "\n# content1=[\n$content\n]\n";
+#print STDERR "\n# is_html=".$mech->is_html().", content type=".$mech->content_type()."\n";
+#print STDERR "\n# dump headers=".$mech->dump_headers."\n";
+
+	return $self->handler("Failed to find that book on the Waterstones website. [$isbn]")
+		if(!$link || $content =~ m!Sorry, there are no results for!si);
+
+    $link =~ s/&amp;/&/g;
+#print STDERR "\n# link3=[$link]\n";
+
+    eval { $mech->get( $link ) };
+    return $self->handler("The Waterstones website appears to be unavailable.")
+	    if($@ || !$mech->success() || !$mech->content());
+
+	# The Book page
+    my $html = $mech->content();
+
+	return $self->handler("Failed to find that book on the Waterstones website. [$isbn]")
+		if($html =~ m!Sorry, there are no results for!si);
+    
+    $html =~ s/&amp;/&/g;
+#print STDERR "\n# content2=[\n$html\n]\n";
+
+    my $data;
+    ($data->{title})            = $html =~ m!<div class="contentProductDetails">\s*<h1>([^<]+)</h1>!si;
+    ($data->{author})           = $html =~ m!<p class="byAuthor">\s*by\s*<a[^>]+>([^<]+)</a>!si;
+    ($data->{binding},$data->{pages})          
+                                = $html =~ m!<td headers="productFormat">([^<]+)\s+(\d+)\s+pages</td>!si;
+    ($data->{description})      = $html =~ m!<h2>Synopsis</h2>\s*<p>([^<]+)!si;
+    ($data->{pubdate})          = $html =~ m!<p><strong>Published</strong><BR />([^<]+)</p>!si;
+    ($data->{publisher})        = $html =~ m!<p><strong>Publisher</strong><BR />([^<]+)</p>!si;
+    ($data->{isbn13})           = $html =~ m!<p><strong>ISBN</strong><BR />([^<]+)</p>!si;
+    ($data->{image})            = $html =~ m!<img src="([^"]+$ean.jpg)"!si;
+
+    $data->{publisher}  =~ s/&#0?39;/'/g;
+    $data->{isbn10}     = $self->convert_to_isbn10($ean);
+    $data->{thumb}      = $thumb || $data->{image};
+
+#use Data::Dumper;
+#print STDERR "\n# " . Dumper($data);
+
+	return $self->handler("Could not extract data from the Waterstones result page.")
+		unless(defined $data);
+
+	# trim top and tail
+	foreach (keys %$data) { 
+        next unless(defined $data->{$_});
+        $data->{$_} =~ s!&nbsp;! !g;
+        $data->{$_} =~ s/^\s+//;
+        $data->{$_} =~ s/\s+$//;
+    }
+
+    my $url = $mech->uri();
+    $url =~ s/\?.*//;
+
+	my $bk = {
+		'ean13'		    => $data->{isbn13},
+		'isbn13'		=> $data->{isbn13},
+		'isbn10'		=> $data->{isbn10},
+		'isbn'			=> $data->{isbn13},
+		'author'		=> $data->{author},
+		'title'			=> $data->{title},
+		'book_link'		=> $url,
+		'image_link'	=> $data->{image},
+		'thumb_link'	=> $data->{thumb},
+		'description'	=> $data->{description},
+		'pubdate'		=> $data->{pubdate},
+		'publisher'		=> $data->{publisher},
+		'binding'	    => $data->{binding},
+		'pages'		    => $data->{pages},
+	};
+
+#use Data::Dumper;
+#print STDERR "\n# book=".Dumper($bk);
+
+    $self->book($bk);
+	$self->found(1);
+	return $self->book;
+}
+
+=item C<convert_to_ean13()>
+
+Given a 10/13 character ISBN, this function will return the correct 13 digit
+ISBN, also known as EAN13.
+
+=item C<convert_to_isbn10()>
+
+Given a 10/13 character ISBN, this function will return the correct 10 digit 
+ISBN.
+
+=back
+
+=cut
+
+sub convert_to_ean13 {
+	my $self = shift;
+    my $isbn = shift;
+    my $prefix;
+
+    return  unless(length $isbn == 10 || length $isbn == 13);
+
+    if(length $isbn == 13) {
+        return  if($isbn !~ /^(978|979)(\d{10})$/);
+        ($prefix,$isbn) = ($1,$2);
+    } else {
+        return  if($isbn !~ /^(\d{10}|\d{9}X)$/);
+        $prefix = '978';
+    }
+
+    my $isbn13 = '978' . $isbn;
+    chop($isbn13);
+    my @isbn = split(//,$isbn13);
+    my ($lsum,$hsum) = (0,0);
+    while(@isbn) {
+        $hsum += shift @isbn;
+        $lsum += shift @isbn;
+    }
+
+    my $csum = ($lsum * 3) + $hsum;
+    $csum %= 10;
+    $csum = 10 - $csum  if($csum != 0);
+
+    return $isbn13 . $csum;
+}
+
+sub convert_to_isbn10 {
+	my $self = shift;
+    my $ean  = shift;
+    my ($isbn,$isbn10);
+
+    return  unless(length $ean == 10 || length $ean == 13);
+
+    if(length $ean == 13) {
+        return  if($ean !~ /^(?:978|979)(\d{9})\d$/);
+        ($isbn,$isbn10) = ($1,$1);
+    } else {
+        return  if($ean !~ /^(\d{9})[\dX]$/);
+        ($isbn,$isbn10) = ($1,$1);
+    }
+
+	return  if($isbn < 0 or $isbn > 999999999);
+
+	my ($csum, $pos, $digit) = (0, 0, 0);
+    for ($pos = 9; $pos > 0; $pos--) {
+        $digit = $isbn % 10;
+        $isbn /= 10;             # Decimal shift ISBN for next time 
+        $csum += ($pos * $digit);
+    }
+    $csum %= 11;
+    $csum = 'X'   if ($csum == 10);
+    return $isbn10 . $csum;
+}
+
+1;
+
+__END__
+
+=head1 REQUIRES
+
+Requires the following modules be installed:
+
+L<WWW::Scraper::ISBN::Driver>,
+L<WWW::Mechanize>
+
+=head1 SEE ALSO
+
+L<WWW::Scraper::ISBN>,
+L<WWW::Scraper::ISBN::Record>,
+L<WWW::Scraper::ISBN::Driver>
+
+=head1 BUGS, PATCHES & FIXES
+
+There are no known bugs at the time of this release. However, if you spot a
+bug or are experiencing difficulties that are not explained within the POD
+documentation, please send an email to barbie@cpan.org or submit a bug to the
+RT system (http://rt.cpan.org/Public/Dist/Display.html?Name=WWW-Scraper-ISBN-Waterstones_Driver).
+However, it would help greatly if you are able to pinpoint problems or even
+supply a patch.
+
+Fixes are dependant upon their severity and my availablity. Should a fix not
+be forthcoming, please feel free to (politely) remind me.
+
+=head1 AUTHOR
+
+  Barbie, <barbie@cpan.org>
+  Miss Barbell Productions, <http://www.missbarbell.co.uk/>
+
+=head1 COPYRIGHT & LICENSE
+
+  Copyright (C) 2010 Barbie for Miss Barbell Productions
+
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic Licence v2.
+
+=cut
---
+++ t/10object.t
@@ -0,0 +1,125 @@
+#!/usr/bin/perl -w
+use strict;
+
+use lib './t';
+use Test::More tests => 34;
+use WWW::Scraper::ISBN;
+
+###########################################################
+
+my $DRIVER          = 'Waterstones';
+my $CHECK_DOMAIN    = 'www.google.com';
+
+my %tests = (
+    '1558607013' => [
+        [ 'is',     'isbn',         '9781558607019'     ],
+        [ 'is',     'isbn10',       '1558607013'        ],
+        [ 'is',     'isbn13',       '9781558607019'     ],
+        [ 'is',     'ean13',        '9781558607019'     ],
+        [ 'is',     'title',        'Higher-Order Perl: Transforming Programs with Programs'            ],
+        [ 'is',     'author',       'Mark Jason Dominus'   ],
+        [ 'is',     'publisher',    'Morgan Kaufmann Publishers In'    ],
+        [ 'is',     'pubdate',      '10/12/2004' ],
+        [ 'is',     'binding',      'Paperback'         ],
+        [ 'is',     'pages',        600                 ],
+        [ 'is',     'image_link',   'http://www.waterstones.com/wat/images/nbd/m/978155/860/9781558607019.jpg' ],
+        [ 'is',     'thumb_link',   'http://www.waterstones.com/wat/images/nbd/s/978155/860/9781558607019.jpg' ],
+        [ 'like',   'description',  qr|Most Perl programmers were originally trained as C and Unix programmers,| ],
+        [ 'is',     'book_link',    'http://www.waterstones.com/waterstonesweb/products/mark+jason+dominus/higher-order+perl/4263640/' ]
+    ],
+    '9780571239566' => [
+        [ 'is',     'isbn',         '9780571239566'     ],
+        [ 'is',     'isbn10',       '0571239560'        ],
+        [ 'is',     'isbn13',       '9780571239566'     ],
+        [ 'is',     'ean13',        '9780571239566'     ],
+        [ 'is',     'title',        'Touching from a Distance'  ],
+        [ 'is',     'author',       'Deborah Curtis'    ],
+        [ 'is',     'publisher',    'Faber and Faber'   ],
+        [ 'is',     'pubdate',      '04/10/2007'   ],
+        [ 'is',     'binding',      'Paperback'         ],
+        [ 'is',     'pages',        240                 ],
+        [ 'is',     'image_link',   'http://www.waterstones.com/wat/images/nbd/m/978057/123/9780571239566.jpg' ],
+        [ 'is',     'thumb_link',   'http://www.waterstones.com/wat/images/nbd/s/978057/123/9780571239566.jpg' ],
+        [ 'like',   'description',  qr|Ian Curtis left behind a legacy rich in artistic genius| ],
+        [ 'is',     'book_link',    'http://www.waterstones.com/waterstonesweb/products/deborah+curtis/touching+from+a+distance/5963509/' ]
+    ],
+);
+
+my $tests = 0;
+for my $isbn (keys %tests) { $tests += scalar( @{ $tests{$isbn} } ) + 2 }
+
+
+###########################################################
+
+my $scraper = WWW::Scraper::ISBN->new();
+isa_ok($scraper,'WWW::Scraper::ISBN');
+
+SKIP: {
+	skip "Can't see a network connection", $tests+1   if(pingtest($CHECK_DOMAIN));
+
+	$scraper->drivers($DRIVER);
+
+    # this ISBN doesn't exist
+	my $isbn = "0987654321";
+    my $record;
+    eval { $record = $scraper->search($isbn); };
+    if($@) {
+        like($@,qr/Invalid ISBN specified/);
+    }
+    elsif($record->found) {
+        ok(0,'Unexpectedly found a non-existent book');
+    } else {
+		like($record->error,qr/Invalid ISBN specified|Failed to find that book|website appears to be unavailable/);
+    }
+
+    for my $isbn (keys %tests) {
+        $record = $scraper->search($isbn);
+        my $error  = $record->error || '';
+
+        SKIP: {
+            skip "Website unavailable", scalar(@{ $tests{$isbn} }) + 2   
+                if($error =~ /website appears to be unavailable/);
+            skip "Book unavailable", scalar(@{ $tests{$isbn} }) + 2   
+                if($error =~ /Failed to find that book/ || !$record->found);
+
+            unless($record->found) {
+                diag($record->error);
+            }
+
+            is($record->found,1);
+            is($record->found_in,$DRIVER);
+
+            my $book = $record->book;
+            for my $test (@{ $tests{$isbn} }) {
+                if($test->[0] eq 'ok')          { ok(       $book->{$test->[1]},             ".. '$test->[1]' found [$isbn]"); } 
+                elsif($test->[0] eq 'is')       { is(       $book->{$test->[1]}, $test->[2], ".. '$test->[1]' found [$isbn]"); } 
+                elsif($test->[0] eq 'isnt')     { isnt(     $book->{$test->[1]}, $test->[2], ".. '$test->[1]' found [$isbn]"); } 
+                elsif($test->[0] eq 'like')     { like(     $book->{$test->[1]}, $test->[2], ".. '$test->[1]' found [$isbn]"); } 
+                elsif($test->[0] eq 'unlike')   { unlike(   $book->{$test->[1]}, $test->[2], ".. '$test->[1]' found [$isbn]"); }
+
+            }
+
+            #use Data::Dumper;
+            #diag("book=[".Dumper($book)."]");
+        }
+    }
+}
+
+###########################################################
+
+# crude, but it'll hopefully do ;)
+sub pingtest {
+    my $domain = shift or return 0;
+    my $cmd =   $^O =~ /solaris/i                           ? "ping -s $domain 56 1" :
+                $^O =~ /dos|os2|mswin32|netware|cygwin/i    ? "ping -n 1 $domain "
+                                                            : "ping -c 1 $domain >/dev/null 2>&1";
+
+    eval { system($cmd) }; 
+    if($@) {                # can't find ping, or wrong arguments?
+        diag();
+        return 1;
+    }
+
+    my $retcode = $? >> 8;  # ping returns 1 if unable to connect
+    return $retcode;
+}
